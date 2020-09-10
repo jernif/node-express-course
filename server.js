4129e0eace9b6565f229a548c6e12dab52bd19a0
jernif <70948327+jernif@users.noreply.github.com>
2020-09-10 11:29:47 +0400

    add first GET route

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const  mockUserData = [
 	{ nom : 'Jill' } 
 ]
 
-app . get ( '/ users/:id' , function ( req , res ) { 
-    console.log(req. params. id)
+app . get ( '/ users' , function ( req , res ) { 
 	res . json ( { 
 		success : true , 
 		message : 'a réussi à obtenir des utilisateurs. Nice!' , 
-		users : req. params. id
+		users : mockUserData 
 	} ) 
 } )
 
+app . get ( '/ users /: id' , function ( req , res ) { 
+	console . log ( req . params . id ) 
+	res . json ( { 
+		success : true , 
+		message : 'got one user' , 
+		user : req . params . id 
+	} ) 
+} )
 app . listen ( 8000 , function ( ) { console . log ( 'serveur écoute' ) } )
