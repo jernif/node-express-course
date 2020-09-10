638e4d558bf744db935aee097b114721a8882114
jernif <70948327+jernif@users.noreply.github.com>
2020-09-10 10:48:49 +0400

    set up express server

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require ( 'express' ) ;
 const app = express () ;
+
+const  mockUserData = [ 
+{ name : 'Mark' } , 
+{ name : 'Jill' } 
+] 
+app . get ( '/ users' ,  function ( req , res ) { 
+ 	res . json ( { 
+ 	 	success : true , 
+ 	 	message : 'a réussi à obtenir des utilisateurs. Nice!' , 
+ 	 	users : mockUserData 
+ 	} ) 
+} )
+
 app . listen ( 8000 , function ( ) { 
 console . log ( "server is running" ) 
 } )
